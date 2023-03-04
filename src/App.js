4c616eacb046211868d730e8e71defad689a0143
fayas8086 <fayasjabbar12345@gmail.com>
2023-03-04 22:42:52 +0530

    netfilx

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Row from './components/Row';
+import './App.css';
+import requests from './request';
+import Banner from './components/Banner';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Nav/>
+      </header>
+      <Banner fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row  
+      isLargeRow={true}
+      title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending" fetchUrl={requests.fetchTrending}/>
+      <Row title="TopRated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="ActionMovies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="ComedyMovies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="HorrorMovies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="RomanceMovies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,24 @@
+import React,{useState,useEffect} from 'react'
+import './Nav.css';
+
+function Nav() {
+    const[show,setShow]=useState(false)
+    useEffect(()=>{
+        window.addEventListener('scroll',()=>{
+            if(window.scrolly>500){
+                setShow(true)
+            }
+            else{
+                setShow(false)
+            }
+        })
+    },[])
+  return (
+    <div className={`nav ${show&&"nav_black"}`}>
+        <img className='nav-img' src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png'>
+        </img>
+    </div>
+  )
+}
+
+export default Nav
